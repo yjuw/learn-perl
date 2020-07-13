ef674e774f373ce76316f864c8d45219ec878597
Brian Kim <byk229@uw.edu>
2020-07-12 22:39:11 -0400

    setup finished

---
+++ Tutorial/Hello.pl
@@ -0,0 +1,18 @@
+#/usr/bin/perl
+use strict;
+use warnings;
+
+print "hi Brian\n";
+
+{
+    #this is code block
+    print "This is\n";
+    print "a block\n";
+    print "of statements.\n";
+}
+
+print("here ", "we ", "print "), "several ", "strings.\n";
+
+{
+    print("ALARM \a")
+}
