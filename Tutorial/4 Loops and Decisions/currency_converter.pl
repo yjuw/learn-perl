7bb48fb302a8f6c61ad1e2618f5dcdbf87591254
yjyj <byk299@uw.edu>
2020-07-23 17:35:53 -0400

    started chap 4

---
+++ Tutorial/4 Loops and Decisions/currency_converter.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my ($value, $from, $to, $rate, %rates);
+
+%rates = (
+    pounds          => 1,
+    dollars         => 1.6,
+    marks           => 3.0,
+    "french francs" => 2.43,
+    yen             => 174.8,
+    "swiss francs"  => 2.43,
+    drachman        => 492.3,
+    euro            => 1.5 
+);
+
+print "Enter starting currency: ";
+$from = <STDIN>;
+
+print "Enter target currency: ";
+$to = <STDIN>;
+
+print "Enter amount ";
+$value = <STDIN>;
+
+chomp($from, $to, $value);
+$rate = $rates{$to} / $rates{$from};
+
+print "$value $from is ", $value*$rate, " $to.\n";
