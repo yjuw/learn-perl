f888f1206fcc0fdd973daabb2c3333df213637f0
yjyj <yjyj@yj-thinkpad.cable.rcn.com>
2020-07-23 11:19:10 -0400

    chapter3 checkpoint

--- Tutorial/3 Lists and Hashes/adding_to_array.pl
+++ Tutorial/3 Lists and Hashes/adding_to_array.pl
@@ -6,3 +6,12 @@ my @array1 = (1,2,3);
 my @array2;
 @array2 = (@array1, 4, 5, 6);
 print "@array2\n";
+
+print "\n";
+print @array1;
+print "\n";
+
+$a = (@array1)[0];
+
+print $a;
+
---
+++ Tutorial/3 Lists and Hashes/extracting_elements.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my @array = (1, 3, 5, 7, 9);
+
+my $subscript = 3;
+
+print $array[$subscript], "\n";
+
+$array[$subscript] = 12;
