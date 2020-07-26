257a05863eed26b6dfbbb1bad1d91451822a8b01
yjyj <byk299@uw.edu>
2020-07-25 23:56:20 -0400

    basic regex

--- Tutorial/5 Regular Expressions/interpolation.pl
+++ Tutorial/5 Regular Expressions/interpolation.pl
@@ -1,3 +1,16 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+
+$_ = q(I wonder what the Entish is for 'yes', and 'no'," he thought.);
+
+print "Enter some text to find: ";
+my $pattern = <STDIN>;
+chomp($pattern);
+
+if (/$pattern/){
+    print "The text matches the pattern '$pattern'.\n";
+    }
+    else{
+        print "'$pattern' was not found\n";
+    }
