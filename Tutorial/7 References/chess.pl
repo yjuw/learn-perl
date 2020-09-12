1448a55c7b4cbb409f36d9f113f4e6cfc86200fe
yjyj <byk299@uw.edu>
2020-09-11 19:39:17 -0700

    finished exercise, solution could be too simple?

--- Tutorial/7 References/chess.pl
+++ Tutorial/7 References/chess.pl
@@ -4,50 +4,60 @@ use strict;
 
 my @chessboard;
 my @back = qw(R N B Q K N B R);
-
 for (0..7) {
-    $chessboard[0] -> [$_] = "W" . $back[$_]; #White Back Row
-    $chessboard[1] -> [$_] = "WP";            #White Pawns
-    $chessboard[6] -> [$_] = "BP";            #Black Pawns
-    $chessboard[7] -> [$_] = "B" . $back[$_]; #Black Back Row
-}
-
-while (1) {
-    #Print board
-    for my $i (reverse (0..7)) {#Row
-        for my $j (0..7) { #Column
-            if (defined $chessboard[$i] -> [$j]){
-                print $chessboard[$i] -> [$j];
-            }
-            elsif ( ($i %2) == ($j %2)) {
-                print "..";
-            }
-            else {
-                print "  ";
-            }
-            print " "; #End of cell
-            }
-            print "\n"; #End of row
+    $chessboard[0]->[$_] = "W" . $back[$_]; # White Back Row
+    $chessboard[1]->[$_] = "WP"; # White Pawns
+    $chessboard[6]->[$_] = "BP"; # Black Pawns
+    $chessboard[7]->[$_] = "B" . $back[$_]; # Black Back Row
     }
-
-    print "\nStarting square [x,y]: ";
-    my $move = <>;
-    last unless ($move =~ /^\s*([1-8]),([1-8])/);
-    #change regular coordinates to perl coordinates (0 is the first reference)
-    my $startx = $1-1; my $starty = $2-1;
-
-    unless (defined $chessboard[$starty] -> [$startx]){
+while (1) {
+    # Print board
+    for my $i (reverse (0..7)) { # Row
+        for my $j (0..7) { # Column
+            if (defined $chessboard[$i]->[$j]) {
+                print $chessboard[$i]->[$j];
+            } 
+            elsif ( ($i %2) == ($j %2) ) {
+        print "..";
+            } 
+            else {
+                print " ";
+            }
+        print " "; # End of cell
+        }
+        print "\n"; # End of row
+        }
+        print "\nStarting square [x,y]: ";
+        my $move = <>;
+        last unless ($move =~ /^\s*([1-8]),([1-8])/);
+        my $startx = $1-1; my $starty = $2-1;
+        unless (defined $chessboard[$starty]->[$startx]) {
         print "There's nothing on that square!\n";
         next;
-    }
-    print "\nEnding square [x,y]: ";
-    $move = <>;
-    last unless ($move =~ /([1-8]), ([1-8])/);
-    my $endx = $1-1;
-    my $endy = $2-1;
+        }
+        
+        print "\nEnding square [x,y]: ";
+        $move = <>;
+        last unless ($move =~ /([1-8]),([1-8])/);
+        my $endx = $1-1; my $endy = $2-1;
 
-    #Put starting square on ending square.
-    $chessboard[$endy] -> [$endx] = $chessboard[$starty] -> [$startx];
-    #Remove from old square
-    undef $chessboard[$starty] -> [$startx];
+        #Check for knight piece
+        my $chosen_piece = $chessboard[$starty]->[$startx];
+        if (($chosen_piece eq "WK") or ($chosen_piece eq "BK")){
+            # print(abs($startx-$endx)**2);
+            # print "\n";
+            # print(abs($starty-$endy)**2);
+            # print "\n";
+            # print(sqrt(13));
+            # print "\n", sqrt(abs($startx-$endx)**2 + abs($starty-$endy)**2);  
+
+            last unless (sqrt( abs($startx-$endx)**2 + abs($starty-$endy**2))) == sqrt(5);
+        }
+
+        #check if it violates rules
+
+        # Put starting square on ending square.
+        $chessboard[$endy]->[$endx] = $chessboard[$starty]->[$startx];
+        # Remove from old square
+        undef $chessboard[$starty]->[$startx];
 }
