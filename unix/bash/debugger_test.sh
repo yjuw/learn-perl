f508c343f2f93ac6c10aefc964889862e50a5e43
yjyj <byk299@uw.edu>
2020-11-14 22:53:06 -0800

    bash user profiles

---
+++ unix/bash/debugger_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+clear
+echo "Script starts now"
+
+echo "hi, $USER!"
+echo
+
+echo "I will now fetch you a list of connected users:"
+echo
+w
+echo
+
+echo "Setting up variables"
+COLOUR="black"
+VALUE="9"
+echo "This is a string: $COLOUR"
+echo "And this is a number: $VALUE"
+echo
+set +x
+echo "I'm giving you back your prompt now"
+echo
---
+++ unix/bash/show_home_services.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#shows where home is dorothy
+echo $HOME
+
+#shows terminal type
+echo $TERM
+
+
+#show runlevel 3 services
+ls /etc/rc3.d/S*
